943aadf9f399c7697fcebaec795a9b0defc1a860
Servi Huijbregts <servihuijbregts@Servis-MacBook-Pro.local>
2023-05-17 13:45:03 +0200

    Changed titles

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -60,8 +60,8 @@ const Experience = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={styles.sectionSubText}>What I have done so far</p>
-        <h2 className={styles.sectionHeadText}>Work Experience.</h2>
+        <p className={styles.sectionSubText}>Wat heb ik tot nu toe gedaan?</p>
+        <h2 className={styles.sectionHeadText}>Stage Tijdlijn.</h2>
       </motion.div>
 
       <div className="mt-20 flex flex-col">
